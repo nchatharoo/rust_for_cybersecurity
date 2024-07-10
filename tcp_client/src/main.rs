c35935b1f1bc0b7d1fe09e6dc5c5ad09fcbdcbc2
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-07-10 22:52:58 +0200

    Create a tcp client

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let target_host = "192.168.1.23";
+    let target_port = 80;
+
+    let mut client = TcpStream::connect(format!("{}:{}", target_host, target_port))?;
+
+    client.write_all(b"ABCDEF")?;
+
+    let mut buffer = [0; 4096];
+    let n = client.read(&mut buffer)?;
+
+    println!("{}", String::from_utf8_lossy(&buffer[..n]));
+    Ok(())
 }
