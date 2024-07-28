f521ba124084102dadc2489725ddf280975fc45d
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-07-28 11:25:26 +0200

    continue to build the scanner

--- scanner/src/main.rs
+++ scanner/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rayon::prelude::*;
+mod subdomains;
+mod ports;
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let http_timeout= Duration::from_secs(10);
+    let http_client = Client::builder().timeout(http_timeout).build()?;
+    let ports_concurrency = 200;
+    let subdomains_concurrency = 100;
+    let scan_start = Instant::now();
+
+    let subdomains = subdomains::enumerate(&http_client, target).await?;
+
 }
